3aff1ca151980d5a88b5e518722e0f3e1d151a92
popcorn0118 <popcorn0118@gmail.com>
2026-07-21 17:22:39 +0800

    拿掉客製化的輪播

--- wp-content/themes/arkhe/inc/enqueue_scripts.php
+++ wp-content/themes/arkhe/inc/enqueue_scripts.php
@@ -45,7 +45,7 @@ function enqueue_front_scripts() {
 	wp_localize_script( 'arkhe-main-script', 'arkheVars', \Arkhe::get_front_global_vars() );
 	wp_dequeue_style( 'classic-theme-styles' );
 
-	// 【比較用／複製版】首頁輪播 Slick.js 版（Slick 官方 CSS 不額外引入，樣式寫在 main.css 裡）
+	// 首頁輪播（Slick.js；官方 CSS 不額外引入，樣式寫在 main.css 裡）
 	if ( is_home() && ! is_paged() ) {
 		wp_enqueue_script( 'slick', ARKHE_THEME_URI . '/vendor/slick/slick.js', array( 'jquery' ), \Arkhe::$file_ver, true );
 		wp_add_inline_script( 'slick', "
